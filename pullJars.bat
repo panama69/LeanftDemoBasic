5d86c8f4aa365f09eb6af8ec17a1328000f70faa
panama69 <github@flynnshome.com>
2016-09-25 18:02:32 -0600

    large change moved where jars are placed for use

--- pullJars.bat
+++ pullJars.bat
@@ -9,38 +9,83 @@ msiexec.exe /i utilities\curl-7.46.0-win64-local.msi /passive
 echo ##
 echo ## Pulling and extracing jars to work with Excel
 echo ##
-mkdir Apache_POI
-cd Apache_POI
+mkdir %userprofile%\LeanftBasicDemoJars
+mkdir %userprofile%\LeanftBasicDemoJars\Apache_POI
+cd %userprofile%\LeanftBasicDemoJars\Apache_POI
 
 %appdata%\..\Local\Apps\cURL\bin\curl.exe -O http://www-us.apache.org/dist/poi/release/bin/poi-bin-3.15-20160924.zip
 
 unzip -jo poi-bin-3.15-20160924.zip poi-3.15/poi*3.15.jar
 del poi-bin-3.15-20160924.zip
-copy *.jar ..\TestExecution
-cd ..
+rem copy *.jar ..\TestExecution
+rem cd ..
 
 
 echo ##
 echo ## Pulling and extracing jars to work with Cucumber
 echo ##
-mkdir cucumber_jars
-cd cucumber_jars
+mkdir %userprofile%\LeanftBasicDemoJars\cucumber_jars
+cd %userprofile%\LeanftBasicDemoJars\cucumber_jars
 %appdata%\..\Local\Apps\cURL\bin\curl.exe -O http://central.maven.org/maven2/info/cukes/cucumber-core/1.2.2/cucumber-core-1.2.2.jar
 %appdata%\..\Local\Apps\cURL\bin\curl.exe -O http://central.maven.org/maven2/info/cukes/cucumber-java/1.2.2/cucumber-java-1.2.2.jar
 %appdata%\..\Local\Apps\cURL\bin\curl.exe -O http://central.maven.org/maven2/info/cukes/gherkin/2.12.2/gherkin-2.12.2.jar
 %appdata%\..\Local\Apps\cURL\bin\curl.exe -O http://central.maven.org/maven2/info/cukes/cucumber-junit/1.2.2/cucumber-junit-1.2.2.jar
 %appdata%\..\Local\Apps\cURL\bin\curl.exe -O http://central.maven.org/maven2/info/cukes/cucumber-html/0.2.3/cucumber-html-0.2.3.jar
 %appdata%\..\Local\Apps\cURL\bin\curl.exe -O http://central.maven.org/maven2/info/cukes/cucumber-jvm-deps/1.0.3/cucumber-jvm-deps-1.0.3.jar
-cd ..
 
 echo ##
 echo ## Pulling and extracing jars to work with Selenium
 echo ##
+cd %userprofile%\LeanftBasicDemoJars
 %appdata%\..\Local\Apps\cURL\bin\curl.exe -O http://selenium-release.storage.googleapis.com/2.53/selenium-java-2.53.1.zip
 unzip -o selenium-java-2.53.1.zip
+unzip -o testng-6.9.9.jar
 del selenium-java-2.53.1.zip
 
-copy "C:\Program Files (x86)\HP\Unified Functional Testing\SDK\Java\com.hp.lft.*.jar" TestExecution
+echo ##
+echo ## Check TestNG plugin for Eclipse is installed
+echo ##
+set TESTNG="C:\Users\Administrator\.p2\pool\plugins\org.testng.eclipse_6.9.12.201607091356\lib\testng.jar"
+set JCOMMANDER="C:\Users\Administrator\.p2\pool\plugins\org.testng.eclipse_6.9.12.201607091356\lib\jcommander.jar"
+
+set MISSING=0
+if not exist %TESTNG% (
+   echo NOT FOUND %TESTNG%
+   echo Make sure TestNG installed to Eclipse
+   set MISSING=1
+)
+
+if not exist %JCOMMANDER% (
+   echo NOT FOUND %JCOMMANDER%
+   echo Make sure TestNG installed to Eclipse
+   set MISSING=1
+)
+
+if %MISSING% EQU 1 (
+   echo ##############################################################
+   echo ##############################################################
+   echo ##                                                          ##
+   echo ##                                                          ##
+   echo ##               NOTE THE ABOVE MISSING FILES               ##
+   echo ##             THIS MUST BE FIXED TO DO THE DEMO            ##
+   echo ##                                                          ##
+   echo ##                                                          ##
+   echo ##############################################################
+   echo ##############################################################
+   timeout /t 10 /nobreak
+)
+
+echo ##
+echo ## Copy LeanFT jar files for easier execution from Jenkins
+echo ##
+mkdir %userprofile%\LeanftBasicDemoJars\LftSdk
+copy "C:\Program Files (x86)\HP\Unified Functional Testing\SDK\Java\com.hp.lft.*.jar" %userprofile%\LeanftBasicDemoJars\LftSdk
+
+
+echo ##
+echo ##
+echo ##
+copy LeanftDemoJava\TestNG.xml TestExecution\
 
 echo ##
 echo ## setting up Jenkins
